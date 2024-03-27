0c45adb57b113b52be8e6677321df17e8ab7c859
Mohamad anas fattoum <samaanas30@gmail.com>
2024-03-28 00:00:24 +0100

    start class Post

--- post/models.py
+++ post/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Post(models.Model):
+    POST_TYPE_CHOICES = (
+        ('question','سؤال'),
+        ('post','منشور'),
+        ('article','مقالة'),
+    )
+
+    p_type =  models.CharField(max_length=500, choices=POST_TYPE_CHOICES, verbose_name='نوع ااماشور')
