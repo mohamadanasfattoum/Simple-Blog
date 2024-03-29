f2d6e47985f919ffdf50c6621d089b2cd4719ec4
Mohamad anas fattoum <samaanas30@gmail.com>
2024-03-29 23:21:03 +0100

    add class meta in class post in  models

--- post/models.py
+++ post/models.py
@@ -15,6 +15,10 @@ class Post(models.Model):
     likes = models.IntegerField ( default=0 , verbose_name='الاعجاب')
     dislikes = models.IntegerField ( default=0 , verbose_name='عدم الاعجاب')
 
+    class Meta:
+        verbose_name = 'المنشور'
+        verbose_name_plural = 'المنشورات'
+
 
     def __str__(self):
          return self.title
