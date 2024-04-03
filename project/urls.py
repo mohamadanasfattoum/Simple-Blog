9199277ca00dd299b1c02c33f465bd4db6334217
Mohamad anas fattoum <samaanas30@gmail.com>
2024-04-04 00:03:35 +0200

    start with make views

--- project/urls.py
+++ project/urls.py
@@ -18,9 +18,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from post.views import post_list, post_detail
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('posts/',post_list),
+    #path(''),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
