211a7a93cd876496598f1751617a261161fd3b83
Mohamad anas fattoum <samaanas30@gmail.com>
2024-04-08 02:03:55 +0200

    remove urls.py from project to app post

--- project/urls.py
+++ project/urls.py
@@ -15,22 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from post.views import post_list, post_detail, add_post, edit_post, delete_post
-from post.views2 import PostList, PostDetail
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('posts/',post_list), # views.py
-    path('posts/',PostList.as_view()),
-    path('posts/add',add_post),
-    # path('posts/<int:post_id>',PostDetail), # views.py
-    path('posts/<int:pk>',PostDetail.as_view()), # pk oder slug
-    path('posts/<int:post_id>/edit',edit_post),
-    path('posts/<int:post_id>/delete',delete_post),
-
+    path('', include('post.urls')),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
