657e2b6e4fdf9c6722fb71f344c0a096e2c09f6c
Mohamad anas fattoum <samaanas30@gmail.com>
2024-04-09 03:11:57 +0200

    used cbv Class UpdeatView to edit post

--- post/urls.py
+++ post/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import post_list, post_detail, add_post, edit_post, delete_post
-from .views2 import PostList, PostDetail, CreatePost
+from .views2 import PostList, PostDetail, CreatePost, UpdatePost
 
 
 urlpatterns = [
@@ -11,9 +11,11 @@ urlpatterns = [
     path('posts/add',CreatePost.as_view()),
 
     # path('posts/<int:post_id>',PostDetail), # views.py
-    path('posts/<int:pk>',PostDetail.as_view()), # pk oder slug
+    path('posts/<int:pk>',PostDetail.as_view(), name='post_detail'), # pk oder slug
 
-    path('posts/<int:post_id>/edit',edit_post),
+    # path('posts/<int:post_id>/edit',edit_post),
+    path('posts/<int:pk>/edit',UpdatePost.as_view()),
+
 
     path('posts/<int:post_id>/delete',delete_post)
 ]
--- post/views2.py
+++ post/views2.py
@@ -1,6 +1,7 @@
 # start with generic  classs
 
 from django.views.generic import *
+from django.urls import reverse
 from .models import Post
 
 # ListView, DetailView, CreateView, UpdateView,  DeleteView
@@ -20,3 +21,18 @@ class CreatePost(CreateView):
     model = Post
     fields = '__all__'
     success_url = '/posts/'
+
+
+class UpdatePost(UpdateView):
+    model = Post
+    fields = '__all__'
+    template_name = 'post/edit_post.html'
+    # success_url = '/posts/{pk}'
+    def get_success_url(self):
+        # Retrieve the object being updated
+        obj = self.get_object()
+        
+        # Return the URL with the pk parameter
+        return reverse('post_detail', kwargs={'pk': obj.pk})
+
+        
